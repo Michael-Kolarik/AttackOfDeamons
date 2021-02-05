15cbe08a31d92790107e73822a70ca5b40d66e44
Michael Kolařík <73023573+Michael-Kolarik@users.noreply.github.com>
2021-02-05 17:15:30 +0100

    verze 1

--- move.py
+++ move.py
@@ -2,10 +2,10 @@ import random
 class Move:
     def __init__(self):
         self.__x = 0
-    def take_card(self, take_pocket, index, put_pocket):
+    def take_card(self, index, take_pocket,  put_pocket):
         put_pocket.append(take_pocket[index])
         del(take_pocket[index])
     def take_random_card(self, take_pocket, put_pocket):
         random_card=random.randint(0,len(take_pocket)-1)
-        self.take_card(take_pocket,random_card, put_pocket)
+        self.take_card(random_card, take_pocket, put_pocket)
 
